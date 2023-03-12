2a1798dcfb61798177fd210ec6a476061c375daa
JuliansRecent <juliansrecent@gmail.com>
2023-03-12 16:03:44 -0700

    Module complete. Create terminal based application that uses a for loop to count from the given number to 100.

--- src/main/java/CountingToHundred.java
+++ src/main/java/CountingToHundred.java
@@ -6,5 +6,16 @@ public class CountingToHundred {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
+        // prompt and collect an integer from the user
+        int number = Integer.valueOf(scanner.nextLine());
+
+        // set variables for start point and finish
+        int start = number;
+        int end = 100;
+
+        // set a loop that counts up from the number given to 100
+        for (int i = start; i <= end; i++) {
+            System.out.println(i);
+        }
     }
 }
